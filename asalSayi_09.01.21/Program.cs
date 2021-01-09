65b789f55bb40d83d775346bcce0a72161fd7864
Begum Yolcu <yolcub98@gmail.com>
2021-01-09 20:12:49 +0300

    First commit

---
+++ asalSayi_09.01.21/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asalSayi_09._01._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 2; i < 10001; i++)
+            {
+                bool asalMisin = true;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        asalMisin = false;
+                    }
+                }
+               
+                if (asalMisin)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
